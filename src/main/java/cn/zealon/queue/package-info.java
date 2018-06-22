3041fdd0b1a46cbf31429b2088b91fcce8011143
Zealon <997717740@qq.com>
2018-06-22 12:33:21 +0800

    join 使用.

---
+++ src/main/java/cn/zealon/queue/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * queque 队列
+ *
+ *
+ * @auther: Zealon
+ * @Date: 2018-06-21 21:30
+ */
+package cn.zealon.queue;
---
+++ src/main/java/cn/zealon/thread/cooperate/UseWaitAndNotify.java
@@ -0,0 +1,11 @@
+package cn.zealon.thread.cooperate;
+
+/**
+ * wait() notify() 用例
+ *
+ * @auther: Zealon
+ * @Date: 2018-06-21 20:28
+ */
+public class UseWaitAndNotify {
+
+}
