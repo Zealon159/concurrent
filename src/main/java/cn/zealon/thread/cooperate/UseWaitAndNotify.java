d50a5af89cb210b7e2863c9db738de91af448fc6
Zealon159 <zealon@126.com>
2018-07-03 22:34:11 +0800

    wait notify.

--- src/main/java/cn/zealon/thread/cooperate/UseWaitAndNotify.java
+++ src/main/java/cn/zealon/thread/cooperate/UseWaitAndNotify.java
@@ -6,6 +6,51 @@ package cn.zealon.thread.cooperate;
  * @auther: Zealon
  * @Date: 2018-06-21 20:28
  */
-public class UseWaitAndNotify {
+public class UseWaitAndNotify implements Runnable{
 
+    //秒
+    private int second = 10;
+
+    public UseWaitAndNotify(int second){
+        this.second = second;
+    }
+
+    public synchronized void greenLight() throws InterruptedException {
+        while (true){
+            for(int i=second;i>=0;i--) {
+                Thread.sleep(1000);
+                if(i==0){
+                    System.out.println(Thread.currentThread().getName()+":红灯");
+                    wait();
+                }
+                System.out.println(Thread.currentThread().getName()+":绿灯，倒计时"+i);
+
+            }
+            notify();
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            greenLight();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args){
+        Runnable runnable = new UseWaitAndNotify(5);
+
+        for (int i=0;i<3600;i++) {
+            Thread t1 = new Thread(runnable);
+            t1.setName("南北路");
+
+            Thread t2 = new Thread(runnable);
+            t2.setName("东西路");
+
+            t1.start();
+            t2.start();
+        }
+    }
 }
